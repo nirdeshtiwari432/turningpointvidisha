b8c831c72f6330e2d0f4916dfed6cd316e2a928c
nirdeshtiwari432 <nirdesh26cs081@satiengg.in>
2025-10-27 09:36:31 +0530

    role

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,11 +8,15 @@ import p3 from "../../assets/p3.jpg";
 import p4 from "../../assets/p4.jpg";
 import p5 from "../../assets/p5.jpg";
 
+
 const Hero = () => {
+  
   const navigate = useNavigate();
   const location = useLocation();
   const [showGallery, setShowGallery] = useState(false);
   const [user, setUser] = useState(null); // store session user
+  // Get user info from localStorage
+  const users = JSON.parse(localStorage.getItem("user"));
 
   const photos = [p1, p2, p3, p4, p5];
 
@@ -57,10 +61,10 @@ const Hero = () => {
   }, [location]);
 
   const handleJoinClick = () => {
-    if (!user) {
+    if (!users) {
       // Not logged in → go to login page
       navigate("/login");
-    } else if (user?.role === "admin") {
+    } else if (users?.role === "admin") {
       // Admin logged in
       navigate("/dashboard");
     } else {
